c7a3b0d5f093674a9b1a096c6bcc9ced5ebd6198
joseph27choi <choi.joseph0302@gmail.com>
2023-01-12 19:21:24 -0500

    First Commit

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '05dd2e37e9msha7e97aa23b89757p163882jsn6ac36025c366',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1aba17bbe403307b794ac53bb624b7a8";
